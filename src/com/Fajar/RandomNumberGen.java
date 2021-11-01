5eccc398279ba50e187813b9c08c5be904b135ea
Fajar Indrawan <fajaridn@student.ittelkom-sby.ac.id>
2021-11-01 19:26:59 +0700

    overloaded

--- src/com/Fajar/RandomNumberGen.java
+++ src/com/Fajar/RandomNumberGen.java
@@ -6,4 +6,8 @@ public class RandomNumberGen {
     public int getRandomNumber(){
         return new Random().nextInt();
     }
+
+    public int getRandomNumber(int limit) {
+        return new Random().nextInt(limit);
+    }
 }
