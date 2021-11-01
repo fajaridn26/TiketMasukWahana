f65f31849a2d6b8f49b5dcb62ee08a9107811fef
Fajar Indrawan <fajaridn@student.ittelkom-sby.ac.id>
2021-11-01 19:05:48 +0700

    initial project

---
+++ src/com/Fajar/Main.java
@@ -0,0 +1,8 @@
+package com.Fajar;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("The number i: " + new com.Fajar.RandomNumberGen().getRandomNumber());
+    }
+}
