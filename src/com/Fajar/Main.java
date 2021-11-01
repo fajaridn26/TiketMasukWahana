1d613a7a42cc87dff1cf768a3a2ef12d20fd6fd8
Fajar Indrawan <fajaridn@student.ittelkom-sby.ac.id>
2021-11-01 19:36:02 +0700

    main class using randomnumber

--- src/com/Fajar/Main.java
+++ src/com/Fajar/Main.java
@@ -3,7 +3,6 @@ package com.Fajar;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
-
+        System.out.println("The number i: " + new com.Fajar.RandomNumberGen().getRandomNumber());
     }
 }
