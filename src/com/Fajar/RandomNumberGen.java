3b81731756682e7c337cc3d8c177877820e396fe
Fajar Indrawan <fajaridn@student.ittelkom-sby.ac.id>
2021-11-08 14:01:34 +0700

    change main.java

--- src/com/Fajar/RandomNumberGen.java
+++ src/com/Fajar/RandomNumberGen.java
@@ -3,11 +3,9 @@ package com.Fajar;
 import java.util.Random;
 
 public class RandomNumberGen {
-    public int getRandomNumber(){
+
+    public static int getRandomNumber(){
         return new Random().nextInt();
     }
 
-    public int getRandomNumber(int limit) {
-        return new Random().nextInt(limit);
-    }
 }
